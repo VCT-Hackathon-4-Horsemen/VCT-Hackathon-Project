aabd282c3a25bdb2187aee19f40614b260fbde06
Abhay Singh <246abhaysingh@gmail.com>
2024-10-15 16:42:51 -0700

    fixed header/navbar

--- Project/client/src/Navbar.js
+++ Project/client/src/Navbar.js
@@ -5,7 +5,9 @@ import logo from './images/logo-black.png'
 export default function Navbar() {
     return (
         <nav>
-            <img src={logo} className="nav--logo" />
+            <img src={logo} className="nav--logo" alt="valpal logo" />
+            <h4 className="credit">Powered By: AWS Bedrock</h4>
+
         </nav>
     )
 }
